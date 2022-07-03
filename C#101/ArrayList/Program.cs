190d9a814d18cc945a6b31fb0af1974cff08a7e8
Batuhan Okur <batuhanokur2209@gmail.com>
2022-07-04 02:25:23 +0300

    ArrayList konusu eklendi

---
+++ C#101/ArrayList/Program.cs
@@ -0,0 +1,63 @@
+﻿//System.Collections namespace
+
+using System.Collections;
+
+ArrayList liste = new ArrayList();
+// liste.Add("Ayse");
+// liste.Add(2);
+// liste.Add(true);
+// liste.Add("a");
+
+//verilere erişim
+// Console.WriteLine(liste[1]);
+// foreach (var item in liste)
+// {
+//     Console.WriteLine(item);
+// }
+
+//Addrange
+
+Console.WriteLine("***** Add Range ************");
+// string[] renkler = {"kırmızı","sarı","yesil"};
+List<int> sayilar = new List<int>(){1,8,3,5,7,6};
+
+// liste.AddRange(renkler);
+liste.AddRange(sayilar);
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+//Sort
+
+Console.WriteLine("**** Sort ******");
+
+liste.Sort();
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+// binary search
+
+Console.WriteLine("**** Binary Search ******");
+Console.WriteLine(liste.BinarySearch(8));
+
+//reverse
+Console.WriteLine("**** reverse ******");
+liste.Reverse();
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+//clear
+Console.WriteLine("**** clear ******");
+liste.Clear();
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
