bfc3555dbe17c1e750d423b8a57f09103326365c
Batuhan Okur <batuhanokur2209@gmail.com>
2022-07-04 03:59:11 +0300

    odev2/soru3 eklendi

---
+++ C#101/odev2/soru3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Bir cümle giriniz!");
+string cumle = Console.ReadLine();
+
+Regex rgx = new Regex(@"[aeouiöüAEOUİÖÜ]");
+MatchCollection sesliHarfler = rgx.Matches(cumle);
+
+foreach (var harf in sesliHarfler)
+{
+    Console.WriteLine(harf);
+}
