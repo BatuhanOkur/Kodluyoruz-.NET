22396e42c232bdb98bcb20fe62a7eab6097d52ed
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-04 16:07:41 +0300

    Initialize function added in DataGenerator.cs

---
+++ Mid-Level/Exercises/LinqPractices/DBOperations/DataGenerator.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+
+namespace LinqPractices.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize()
+        {
+            using(var context = new LinqDbContext())
+            {
+                if(context.Students.Any())
+                {
+                    return;
+                }
+                context.Students.AddRange(
+                    new Student(){
+                        StudentId = 1,
+                        Name = "Serkan",
+                        Surname = "Okur",
+                        ClassId = 11
+                    },
+                    new Student(){
+                        StudentId = 2,
+                        Name = "Gül",
+                        Surname = "Okur",
+                        ClassId = 12
+                    },
+                    new Student(){
+                        StudentId = 3,
+                        Name = "Süleyman",
+                        Surname = "Okur",
+                        ClassId = 10
+                    },
+                    new Student(){
+                        StudentId = 1,
+                        Name = "Batuhan",
+                        Surname = "Okur",
+                        ClassId = 9
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
