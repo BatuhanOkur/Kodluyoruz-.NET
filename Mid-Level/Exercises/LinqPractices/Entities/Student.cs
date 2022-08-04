ccfc72db45267c17f74a4a26a19b0b7e24827ac4
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-04 15:57:32 +0300

    LinqPractices added, LinqPractices DBContext created.

---
+++ Mid-Level/Exercises/LinqPractices/Entities/Student.cs
@@ -0,0 +1,11 @@
+namespace LinqPractices
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public int Name { get; set; }
+        public int Surname { get; set; }
+        public int ClassId { get; set; }
+        
+    }
+}
