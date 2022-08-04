f843fc75e653b4eab49c812ad54e0fac0d52617d
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-04 15:23:56 +0300

    DB context added and endpoints edited in BookController.cs.

--- Mid-Level/Exercises/BookStore/WebApi/Controllers/BookController.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.AddControllers
 {
@@ -9,41 +10,46 @@ namespace WebApi.AddControllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>(){
-            new Book{
-                Id = 1,
-                Title = "Türklerin Tarihi",
-                GenreId = 1, //history
-                PageCount = 200,
-                PublishDate = new DateTime(2001,06,12)
-            },
-            new Book{
-                Id = 2,
-                Title = "Herland",
-                GenreId = 2, //Science Fic
-                PageCount = 250,
-                PublishDate = new DateTime(2010,05,23)
-            },
-            new Book{
-                Id = 3,
-                Title = "Dune",
-                GenreId = 2, //Science Fic
-                PageCount = 250,
-                PublishDate = new DateTime(2018,04,13)
-            }
-        };
+        private readonly BookStoreDbContext _context;
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        // private static List<Book> BookList = new List<Book>(){
+        //     new Book{
+        //         Id = 1,
+        //         Title = "Türklerin Tarihi",
+        //         GenreId = 1, //history
+        //         PageCount = 200,
+        //         PublishDate = new DateTime(2001,06,12)
+        //     },
+        //     new Book{
+        //         Id = 2,
+        //         Title = "Herland",
+        //         GenreId = 2, //Science Fic
+        //         PageCount = 250,
+        //         PublishDate = new DateTime(2010,05,23)
+        //     },
+        //     new Book{
+        //         Id = 3,
+        //         Title = "Dune",
+        //         GenreId = 2, //Science Fic
+        //         PageCount = 250,
+        //         PublishDate = new DateTime(2018,04,13)
+        //     }
+        // };
 
         [HttpGet]
         public List<Book> GetBooks()
         {
-            var booklist = BookList.OrderBy(book=> book.Id).ToList<Book>();
+            var booklist = _context.Books.OrderBy(book=> book.Id).ToList<Book>();
             return booklist;
         }
 
         [HttpGet("{id}")]
         public Book GetById(int id)
         {
-            var book = BookList.Where(book=> book.Id == id).SingleOrDefault();
+            var book = _context.Books.Where(book=> book.Id == id).SingleOrDefault();
             return book; 
         }
 
@@ -58,18 +64,19 @@ namespace WebApi.AddControllers
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook)
         {
-            var book = BookList.SingleOrDefault(book => book.Title == newBook.Title);
+            var book = _context.Books.SingleOrDefault(book => book.Title == newBook.Title);
             if(book is not null)
                 return  BadRequest();
 
-            BookList.Add(newBook);
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
-            var book = BookList.SingleOrDefault(book => book.Id == id);
+            var book = _context.Books.SingleOrDefault(book => book.Id == id);
             if(book is null)
                 return BadRequest();
             
@@ -77,17 +84,19 @@ namespace WebApi.AddControllers
             book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
             book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
             book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = BookList.SingleOrDefault(book => book.Id == id);
+            var book = _context.Books.SingleOrDefault(book => book.Id == id);
             if(book is null)
                 return BadRequest();
             
-            BookList.Remove(book);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
             return Ok();
         }
 
