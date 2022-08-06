34075cbde5294eaf259b013f833e6622c65fd025
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-06 16:05:35 +0300

    The way it maps the model to the entity has been changed in CreateBookCommand.cs.

--- Mid-Level/Exercises/BookStore/WebApi/BookOperations/CreateBook/CreateBookCommand.cs
+++ Mid-Level/Exercises/BookStore/WebApi/BookOperations/CreateBook/CreateBookCommand.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using AutoMapper;
 using WebApi.DBOperations;
 namespace WebApi.BookOperations.CreateBook
 {
@@ -7,20 +8,22 @@ namespace WebApi.BookOperations.CreateBook
     {
         public CreateBookModel Model {get; set;}
         public readonly BookStoreDbContext _dbContext;
-        public  CreateBookCommand(BookStoreDbContext dbContext)
+        public readonly IMapper _mapper;
+        public  CreateBookCommand(BookStoreDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
         public void Handle()
         {
             var book = _dbContext.Books.SingleOrDefault(x => x.Title == Model.Title);
             if (book is not null)
                 throw new InvalidOperationException("Kitap zaten mevcut!");
-            book = new Book();
-            book.Title = Model.Title;
-            book.GenreId = Model.GenreId;
-            book.PageCount = Model.PageCount;
-            book.PublishDate = Model.PublishDate;
+            book = _mapper.Map<Book>(Model); //new Book();
+            // book.Title = Model.Title;
+            // book.GenreId = Model.GenreId;
+            // book.PageCount = Model.PageCount;
+            // book.PublishDate = Model.PublishDate;
 
             _dbContext.Add(book);
             _dbContext.SaveChanges();
