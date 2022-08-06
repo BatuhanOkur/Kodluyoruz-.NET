72c245a38f3106293fce203949ce2e38d63363ee
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-06 14:29:09 +0300

    DeleteBookCommand.cs created, DeleteBook endpoint in BookController.cs updated

--- Mid-Level/Exercises/BookStore/WebApi/Controllers/BookController.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.DeleteBook;
 using WebApi.BookOperations.GetBookDetail;
 using WebApi.BookOperations.GetBooks;
 using WebApi.BookOperations.UpdateBook;
@@ -89,12 +90,17 @@ namespace WebApi.AddControllers
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = _context.Books.SingleOrDefault(book => book.Id == id);
-            if(book is null)
-                return BadRequest();
-            
-            _context.Books.Remove(book);
-            _context.SaveChanges();
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            try
+            {
+                command.BookId = id;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
             return Ok();
         }
 
