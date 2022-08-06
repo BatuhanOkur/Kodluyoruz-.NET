a52327d5140e5ac9b68343eb8bea23435223a5ea
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-06 16:03:23 +0300

    Mapping options are specified in the MappingProfile.cs

---
+++ Mid-Level/Exercises/BookStore/WebApi/Common/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using WebApi.BookOperations.CreateBook;
+
+namespace WebApi.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateBookModel, Book>();
+        }
+    }
+}
