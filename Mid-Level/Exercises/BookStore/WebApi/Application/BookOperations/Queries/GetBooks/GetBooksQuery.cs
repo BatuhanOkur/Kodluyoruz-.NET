f36d059b2d817155338b24357e32aca6d6df57bd
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-14 00:20:26 +0300

    Genre entity has been added to GetBooksQuery

--- Mid-Level/Exercises/BookStore/WebApi/Application/BookOperations/Queries/GetBooks/GetBooksQuery.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Application/BookOperations/Queries/GetBooks/GetBooksQuery.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using WebApi.Common;
 using WebApi.DBOperations;
 using WebApi.Entities;
@@ -20,7 +21,7 @@ namespace WebApi.Application.BookOperations.Queries.GetBooks
 
         public List<BooksViewModel> Handle()
         {
-            var booklist = _dbContext.Books.OrderBy(book=> book.Id).ToList<Book>();
+            var booklist = _dbContext.Books.Include(x => x.Genre).OrderBy(x=> x.Id).ToList<Book>();
             List<BooksViewModel> vm = _mapper.Map<List<BooksViewModel>>(booklist);
             return vm;
         }
