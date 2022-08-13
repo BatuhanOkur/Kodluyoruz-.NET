1bb8de5b2d4bf055f5f897785359f08078dbee8d
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-13 19:20:02 +0300

    HandleException fixed

--- Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -40,6 +40,7 @@ namespace WebApi.Middlewares
         {
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            
             string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message: " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + " ms.";
             Console.WriteLine(message);
 
