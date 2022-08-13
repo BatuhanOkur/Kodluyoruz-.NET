83ccaf5f51bc00a82c71906c0cd0fe84259941b6
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-13 23:28:46 +0300

    GenreController is created

---
+++ Mid-Level/Exercises/BookStore/WebApi/Controllers/GenreController.cs
@@ -0,0 +1,41 @@
+using System;
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.GenreOperations.Queries.GetGenres;
+using WebApi.DBOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class GenreController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GenreController(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetGenres()
+        {
+            GetGenresQuery query = new GetGenresQuery(_context, _mapper);
+            var obj = query.Handle();
+            return Ok(obj);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
