3b8de8cc70938932646b509d161eafb5c75c787c
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-13 18:51:42 +0300

    Response time log has been added to Invoke function in CustomExceptionMiddleware.cs

--- Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -15,9 +16,13 @@ namespace WebApi.Middlewares
 
         public async Task Invoke(HttpContext context)
         {
+            var watch = Stopwatch.StartNew();
             string message = "[Request] HTTP " + context.Request.Method + " - " + context.Request.Path;
             Console.WriteLine(message);
             await _next(context);
+            watch.Stop();
+            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms.";
+            Console.WriteLine(message);
         }
 
     }
