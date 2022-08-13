c57e84dbb513c3002f98f8dcaf0c32943e576323
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-14 00:17:11 +0300

    Mapping options have been changed based on Genre Entity.

--- Mid-Level/Exercises/BookStore/WebApi/Common/MappingProfile.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Common/MappingProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using WebApi.Application.BookOperations.Commands.CreateBook;
 using WebApi.Application.BookOperations.Queries.GetBookDetail;
 using WebApi.Application.BookOperations.Queries.GetBooks;
+using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
 using WebApi.Application.GenreOperations.Queries.GetGenres;
 using WebApi.Entities;
 
@@ -12,9 +13,10 @@ namespace WebApi.Common
         public MappingProfile()
         {
             CreateMap<CreateBookModel, Book>();
-            CreateMap<Book, GetBookDetailModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<Book, GetBookDetailModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
+            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
             CreateMap<Genre,GetGenresViewModel>();
+            CreateMap<Genre,GetGenreDetailViewModel>();
         }
     }
 }
