26bd11b07bf3c9616c2f22ba7266dfa2e94d1ca5
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-13 19:04:49 +0300

    Exception handling with try catch has been added in CustomExceptionsMiddleware.

--- Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ Mid-Level/Exercises/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Diagnostics;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace WebApi.Middlewares
 {
@@ -17,14 +19,37 @@ namespace WebApi.Middlewares
         public async Task Invoke(HttpContext context)
         {
             var watch = Stopwatch.StartNew();
-            string message = "[Request] HTTP " + context.Request.Method + " - " + context.Request.Path;
-            Console.WriteLine(message);
-            await _next(context);
-            watch.Stop();
-            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms.";
-            Console.WriteLine(message);
+            try
+            {
+                string message = "[Request] HTTP " + context.Request.Method + " - " + context.Request.Path;
+                Console.WriteLine(message);
+                await _next(context);
+                watch.Stop();
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms.";
+                Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                watch.Stop();
+                await HandleException(context,ex,watch);
+            }
+            
         }
 
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message: " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + " ms.";
+            Console.WriteLine(message);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = JsonConvert.SerializeObject(new {error = ex.Message}, Formatting.None);
+            return context.Response.WriteAsync(result);
+
+
+
+        }
     }
 
     public static class CustomExceptionMiddlewareExtension
