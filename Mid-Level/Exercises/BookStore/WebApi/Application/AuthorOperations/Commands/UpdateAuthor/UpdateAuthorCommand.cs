bfd931fb450fb113e9afae5e22740634c41dd06d
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-14 16:24:57 +0300

    UpdateAuthorCommand has been completed

---
+++ Mid-Level/Exercises/BookStore/WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebApi.DBOperations;
+
+namespace WebAPI.Applications.AuthorOperations.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommand
+    {
+        public readonly BookStoreDbContext _context;
+        public readonly IMapper _mapper;
+        
+        public int AuthorId { get; set; }
+        public UpdateAuthorViewModel Model { get; set; }
+        public UpdateAuthorCommand(BookStoreDbContext dbContext, IMapper mapper)
+        {
+            _context = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Id == AuthorId);
+            if(author is null)
+                throw new InvalidOperationException("Bu yazar daha önceden eklenmiştir!");
+
+            if(_context.Authors.Any(x => 
+                    new string(x.Name.ToLower().Trim() + " " + x.Surname.ToLower().Trim()) ==
+                    new string(Model.Name.ToLower().Trim() + " " + Model.Surname.ToLower().Trim())
+                     && x.Id != AuthorId))
+            {
+                throw new InvalidOperationException("Bu yazar zaten mevcut!");
+            }
+            
+            author.Name = Model.Name != default ? Model.Name : author.Name;
+            author.Surname = Model.Surname != default ? Model.Surname : author.Surname;
+            author.BirthDate = Model.BirthDate != default ? Model.BirthDate : author.BirthDate;
+            _context.SaveChanges();
+        }
+    }
+
+    public class UpdateAuthorViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
