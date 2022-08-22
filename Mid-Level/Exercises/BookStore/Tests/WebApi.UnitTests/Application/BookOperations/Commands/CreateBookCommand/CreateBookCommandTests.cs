84129e335b57a3d472239e5883451483fb73991c
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-22 17:17:42 +0300

    CreateBookCommand class is inherited from the CommonTestFixture class.

--- Mid-Level/Exercises/BookStore/Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBookCommand/CreateBookCommandTests.cs
+++ Mid-Level/Exercises/BookStore/Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateBookCommand/CreateBookCommandTests.cs
@@ -1,7 +1,11 @@
+using TestSetup;
+using Xunit;
+
+
 namespace Application.BookOperations.Commands.CreateBookCommand
 {
-    public class CreateBookCommandTests
+    public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
     {
-        
+
     }
 }
